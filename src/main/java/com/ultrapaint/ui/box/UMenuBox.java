a5a9b441860aa7608211c91b834bbe2584fecb47
Sardire <smf3141592@gmail.com>
2025-11-24 16:11:38 +0700

    Update some functions for buttons

--- src/main/java/com/ultrapaint/ui/box/UMenuBox.java
+++ src/main/java/com/ultrapaint/ui/box/UMenuBox.java
@@ -2,18 +2,39 @@ package com.ultrapaint.ui.box;
 
 import javafx.scene.control.*;
 import com.ultrapaint.App;
+import javafx.scene.layout.HBox;
 
-public class UMenuBox extends MenuBar {
+public class UMenuBox extends HBox {
     public UMenuBox(App app) {
+        MenuBar menuBar = new MenuBar();
+
         MenuItem newButton = new MenuItem("New");
         MenuItem saveButton = new MenuItem("Save");
+        saveButton.setOnAction(e -> {
+            app.stateManager.saveFile();
+        });
         MenuItem openButton = new MenuItem("Open");
+        openButton.setOnAction(e -> {
+            app.stateManager.openFile();
+        });
         Menu fileMenu = new Menu("File");
         fileMenu.getItems().addAll(newButton, saveButton, openButton);
         
         Menu editMenu = new Menu("Edit");
-        Menu helpMenu = new Menu("Help");
+        Button undoButton = new Button("Undo");
+        Button redoButton = new Button("Redo");
 
-        this.getMenus().addAll(fileMenu, editMenu, helpMenu);
+        undoButton.setOnAction(e -> {
+            app.stateManager.undo();
+        });
+
+        redoButton.setOnAction(e -> {
+            app.stateManager.redo();
+        });
+
+        menuBar.getMenus().addAll(fileMenu, editMenu);
+        HBox buttons = new HBox(undoButton, redoButton);
+
+        this.getChildren().addAll(menuBar, buttons);
     }
 }
